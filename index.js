9446a83b3f6b59a6b7170da37cb6ab94d3a9b2e6
Vlad <vlad@difogic.com>
2016-02-10 17:27:38 +0200

    Fall back to the `readme` from the NPM API response if present

--- index.js
+++ index.js
@@ -15,22 +15,30 @@ function getPackageReadme (pkgName, cb) {
     } catch (err) {
       return cb(new Error(pkgName + ': cannot parse registry data: ' + err.message))
     }
+
+    var checkFallback = function (err) {
+      if (data.readme) {
+        return cb(null, data.readme)
+      }
+      return cb(err)
+    }
+
     var readmeFilename = data.readmeFilename
-    if (!readmeFilename) return cb(new Error(pkgName + ': package.json has no readmeFilename'))
+    if (!readmeFilename) return checkFallback(new Error(pkgName + ': package.json has no readmeFilename'))
 
     var repoUrl = data.repository && data.repository.url
-    if (!repoUrl) return cb(new Error(pkgName + ': package.json has no repository'))
+    if (!repoUrl) return checkFallback(new Error(pkgName + ': package.json has no repository'))
 
     var repoObj = gh(repoUrl)
     var user = repoObj && repoObj.user
     var repo = repoObj && repoObj.repo
-    if (!user || !repo) return cb(new Error(pkgName + ': cannot parse repository url'))
+    if (!user || !repo) return checkFallback(new Error(pkgName + ': cannot parse repository url'))
 
     var readmePath = user + '/' + repo + '/master/' + readmeFilename
     var githubUrl = GITHUB_README.replace('%s', readmePath)
 
     get.concat(githubUrl, function (err, data) {
-      if (err) return cb(err)
+      if (err) return checkFallback(err)
       cb(null, data.toString())
     })
   })
--- test/basic.js
+++ test/basic.js
@@ -4,6 +4,7 @@ var test = require('tape')
 test('get package readme for "webtorrent"', function (t) {
   t.plan(4)
   getPackageReadme('webtorrent', function (err, readme) {
+    if (err) {}; // to make eslint happy
     t.ok(/webtorrent/i.test(readme))
     t.ok(/feross aboukhadijeh/i.test(readme))
     t.ok(/streaming torrent client/i.test(readme))
@@ -14,6 +15,7 @@ test('get package readme for "webtorrent"', function (t) {
 test('get package readme for "browserify"', function (t) {
   t.plan(2)
   getPackageReadme('browserify', function (err, readme) {
+    if (err) {};
     t.ok(/browserify/i.test(readme))
     t.ok(/modules/i.test(readme))
   })
